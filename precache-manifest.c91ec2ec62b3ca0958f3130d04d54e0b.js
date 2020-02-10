d646b95cc463dccd80cbcd77a8f034f56872b294
wangjq4214 <35188480+wangjq4214@users.noreply.github.com>
2020-02-10 15:52:33 +0000

    Deploying to gh-pages from  - 62ffd57f50e9646822d411fe1b4be9b6c2789597 🚀

---
+++ precache-manifest.c91ec2ec62b3ca0958f3130d04d54e0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc9a2673574c8d551811",
+    "url": "./umi.94adfc18.css"
+  },
+  {
+    "revision": "dc9a2673574c8d551811",
+    "url": "./umi.c22c48f2.js"
+  },
+  {
+    "revision": "862b8528f58f20de1b62faf0351ac98a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f647b9e809c9d7d0071eb4ee56c761e",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "./pwacompat.min.js"
+  }
+];
