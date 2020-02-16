0a75db9ab84e6e626b88a50dd1e3cfc5e6e6115b
qkuns <qkuns@qq.com>
2020-02-16 19:48:05 +0800

    fix: useStorageState

--- packages/hooks/src/useStorageState/index.ts
+++ packages/hooks/src/useStorageState/index.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import Taro, { useState } from '@tarojs/taro';
 
 interface IFuncUpdater<T> {
   (previousState?: T): T;
@@ -8,11 +8,12 @@ function isFunction<T>(obj: any): obj is T {
   return typeof obj === 'function';
 }
 
-function useStorageState<T>(storage: Storage, key: string, defaultValue?: T | IFuncUpdater<T>) {
+function useStorageState<T>(key: string, defaultValue?: T | IFuncUpdater<T>) {
   const [state, setState] = useState<T | undefined>(() => getStoredValue());
 
+  // 同步获取 Storage 中的值
   function getStoredValue() {
-    const raw = storage.getItem(key);
+    const raw = Taro.getStorageSync(key);
     if (raw) {
       return JSON.parse(raw);
     }
@@ -24,18 +25,20 @@ function useStorageState<T>(storage: Storage, key: string, defaultValue?: T | IF
 
   function updateState(value?: T | IFuncUpdater<T>) {
     if (typeof value === 'undefined') {
-      storage.removeItem(key);
+      Taro.removeStorageSync(key);
       setState(defaultValue);
     } else if (isFunction<IFuncUpdater<T>>(value)) {
+      // 如果 value 是个 update Function
       const previousState = getStoredValue();
       const currentState = value(previousState);
-      storage.setItem(key, JSON.stringify(currentState));
+      Taro.setStorageSync(key, JSON.stringify(currentState));
       setState(currentState);
     } else {
-      storage.setItem(key, JSON.stringify(value));
+      Taro.setStorageSync(key, JSON.stringify(value));
       setState(value);
     }
   }
+  // 返回一个用于更新 State 和 Storage 的值得函数
   return [state, updateState];
 }
 
